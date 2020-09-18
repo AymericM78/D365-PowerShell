7f9accd0770c9871a6a09339e220cdf98ea7a567
Aymeric Mouillé <aymeric.mouille@gmail.com>
2020-09-18 17:12:46 +0200

    Update D365.DevOps.Powershell.Installer.ps1

--- D365.DevOps.Powershell.Installer.ps1
+++ D365.DevOps.Powershell.Installer.ps1
@@ -16,7 +16,7 @@ $packageMetadata =  $response.Content | ConvertFrom-Json;
 $latestVersion = $packageMetadata.items.upper;
 Write-Host "D365.DevOps.Powershell latest version = $latestVersion" -ForegroundColor Yellow;
 
-$forceVersion = "2020.9.119";
+$forceVersion = "2020.9.1817";
 Write-Host "D365.DevOps.Powershell force version = $forceVersion" -ForegroundColor Yellow;
 $latestVersion = $forceVersion;
 
